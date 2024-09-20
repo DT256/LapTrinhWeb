968c3324b2d2e7d4fcc5c08b1bda9b539e0a5b93
dt256 <buiducthang13022004@gmail.com>
2024-09-20 09:23:05 +0700

    Update service

--- src/main/java/services/IUserService.java
+++ src/main/java/services/IUserService.java
@@ -5,4 +5,10 @@ import models.UserModel;
 public interface IUserService {
 	UserModel findByUserName(String username);
 	UserModel login(String username, String password);
+	UserModel findByUserNameAndEmail(String username, String email);
+	boolean updatePassword(String username, String newPassword);
+	void insertUser(UserModel user);
+	boolean register(String username, String email, String fullname, String password, int roleid);
+	boolean checkExistEmail(String email);
+	boolean checkExistUsername(String username);
 }
--- src/main/java/services/impl/UserServiceImpl.java
+++ src/main/java/services/impl/UserServiceImpl.java
@@ -5,12 +5,13 @@ import dao.impl.UserDaoImpl;
 import models.UserModel;
 import services.IUserService;
 
+import java.util.Date;
+
 public class UserServiceImpl implements IUserService {
     IUserDao userDao = new UserDaoImpl();
 
     @Override
     public UserModel findByUserName(String username) {
-
         return userDao.findByUsername(username);
     }
 
@@ -23,11 +24,61 @@ public class UserServiceImpl implements IUserService {
         return null;
     }
 
+    @Override
+    public UserModel findByUserNameAndEmail(String username, String email) {
+        return userDao.findByUsernameAndEmail(username, email);
+    }
+
+    @Override
+    public boolean updatePassword(String username, String newPassword) {
+        return userDao.updatePassword(username, newPassword);
+    }
+
+    @Override
+    public void insertUser(UserModel user) {
+        userDao.insertUser(user);
+    }
+
+    @Override
+    public boolean register(String username, String email, String fullname, String password, int roleid) {
+        if(userDao.checkExistUsername(username)) {
+            return false;
+        }
+        if(userDao.checkExistEmail(email)) {
+            return false;
+        }
+        UserModel user = new UserModel();
+        user.setUsername(username);
+        user.setFullname(fullname);
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setRoleid(roleid);
+
+        long millis = System.currentTimeMillis();
+        java.sql.Date date = new java.sql.Date(millis);
+        user.setCreateDate(date);
+
+        this.insertUser(user);
+        return true;
+    }
+
+    @Override
+    public boolean checkExistEmail(String email) {
+        return userDao.checkExistEmail(email);
+    }
+
+    @Override
+    public boolean checkExistUsername(String username) {
+        return userDao.checkExistUsername(username);
+    }
+
     public static void main(String[] args) {
         try {
-            IUserService userService = new UserServiceImpl();
-
-            System.out.println(userService.login("thangbd", "1234"));
+            //long millis = System.currentTimeMillis();
+            //Date sqlDate = new Date(millis);
+            //System.out.print(sqlDate);
+            UserServiceImpl userService = new UserServiceImpl();
+            //System.out.println(userService.register("1123","ddd@gmail.com","1234",2));
 
         } catch (Exception e) {
             e.printStackTrace();
