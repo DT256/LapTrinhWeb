ae69e6ba8931911d7c925ab6bd86016aa2ad4d48
dt256 <buiducthang13022004@gmail.com>
2024-09-30 21:27:10 +0700

    Update Constant

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -1,7 +1,10 @@
 package utils;
 
+import java.io.File;
+
 public class Constant {
     public static final String SESSION_USERNAME = "username";
     public static final String COOKIE_REMEMBER = "username";
     public static final String DEFAULT_FILENAME = "default.file";
+    public static final String ULOAD_DIRECTORY = "D:" + File.separator + "upload";
 }
